9e68f1c42a3633266301f994012909e39edfc830
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-05 16:42:12 -0700

    completed functions A

---
+++ 02_functions/A_functions_exercise/snippet-0-3.js
@@ -0,0 +1,25 @@
+// snippet 0-3
+let average = function (num1, num2) {
+  console.log("calculating...");
+  return (num1 + num2) / 2;
+};
+
+console.log(average(5, 10));
+console.log(average(20, 26));
+console.log(average(50, 100) + 2);
+
+let a = 21 + 3;
+let b = 20;
+let n = average(a, b); // 46
+console.log(average(n, 18)); // 54 / 2 = 28
+
+// calculatting...
+// 7.5
+// calculatting...
+// 23
+// calculatting...
+// 77
+// calculatting...
+// 28 -- wrong
+// 20
+
