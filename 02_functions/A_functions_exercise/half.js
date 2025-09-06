b36732e1abca394c46be621cf90f1966497cb820
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-05 18:38:19 -0700

    completed functions A

---
+++ 02_functions/A_functions_exercise/half.js
@@ -0,0 +1,8 @@
+// Write a function `half` that accepts a number as an argument. The function should return half of the
+// number.
+
+const half = n => n / 2
+
+console.log(half(8)); // 4
+console.log(half(15)); // 7.5
+console.log(half(90)); // 45
